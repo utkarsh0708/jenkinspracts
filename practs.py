74590ab352bd3e4083d06e97adba7bb815c884fc
utkarsh0708 <86555024+utkarsh0708@users.noreply.github.com>
2021-10-29 09:26:51 +0530

    Create practs.py

---
+++ practs.py
@@ -0,0 +1,27 @@
+# Method to find the factorial of a number
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+ 
+# Method to compute the sum
+def sum(x, n):
+    total = 1.0
+ 
+    # Iterate the loop till n
+    # and compute the formula
+    for i in range (1, n + 1, 1):
+        total = total + (pow(x, i) / fact(i))
+ 
+    return total
+ 
+# Driver code
+if __name__== '__main__':
+     
+    # Get x and n
+    x = 5
+    n = 4
+ 
+    # Print output
+    print ("Sum is: {0:.4f}".format(sum(x, n)))
